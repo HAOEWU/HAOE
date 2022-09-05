437364a8ec33b403a6cd7a1db47b0de177f49a14
HAOEWU <110072550+HAOEWU@users.noreply.github.com>
2022-09-05 11:11:26 +0800

    Add files via upload

---
+++ 音乐播放器/js/music-list.js
@@ -0,0 +1,14 @@
+let allMusic = [
+    {
+        name: "7 Years", //music name
+        artist: "Conor Manynard", //artist name
+        img: "music-1", //image name
+        src: "music-1" //name here
+    },
+    {
+        name: "到此為止",
+        artist: "老虎歌皇",
+        img: "music-2",
+        src: "music-2"
+    },
+];
